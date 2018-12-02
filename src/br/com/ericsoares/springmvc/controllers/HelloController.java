c97adc0732b38e2cdce6b0bf923309d435d46950
soareseric <contato.eric.sm@gmail.com>
2018-12-02 21:16:42 -0200

    Repassando informacoes das views para os controllers: PathVariable

--- src/br/com/ericsoares/springmvc/controllers/HelloController.java
+++ src/br/com/ericsoares/springmvc/controllers/HelloController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,5 +28,11 @@ public class HelloController {
 	@RequestMapping("/mensagemDoServidorV2")
 	public ModelAndView mensagemDoServidorV2() {
 		return new ModelAndView("mensagemDoServidor", "mensagem", new Date().toString());
+	}
+
+	@RequestMapping("/receberMensagem/{mensagem}")
+	public String receberMensagem(Model model, @PathVariable("mensagem") String msg) {
+		model.addAttribute("mensagem", msg);
+		return "mensagemDoServidor";
 	}
 }
