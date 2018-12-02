e06530bedd3cad3ca838948946003a41053d9462
soareseric <contato.eric.sm@gmail.com>
2018-12-02 21:10:09 -0200

    Repassando informacoes das actions para as views: classe ModelandView

--- src/br/com/ericsoares/springmvc/controllers/HelloController.java
+++ src/br/com/ericsoares/springmvc/controllers/HelloController.java
@@ -1,8 +1,11 @@
 package br.com.ericsoares.springmvc.controllers;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/hello")
@@ -19,5 +22,10 @@ public class HelloController {
 	public String mensagemDoServidor(Model model) {
 		model.addAttribute("mensagem", "Olá, Eric Soares");
 		return "mensagemDoServidor";
+	}
+	
+	@RequestMapping("/mensagemDoServidorV2")
+	public ModelAndView mensagemDoServidorV2() {
+		return new ModelAndView("mensagemDoServidor", "mensagem", new Date().toString());
 	}
 }
