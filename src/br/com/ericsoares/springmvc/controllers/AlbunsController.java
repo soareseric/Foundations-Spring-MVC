0cd45a34afc1f65fa0329b6e74a05bce2ce543bf
soareseric <contato.eric.sm@gmail.com>
2018-12-05 17:01:29 -0200

    Inicio integracao com o controller

--- src/br/com/ericsoares/springmvc/controllers/AlbunsController.java
+++ src/br/com/ericsoares/springmvc/controllers/AlbunsController.java
@@ -1,20 +1,35 @@
 package br.com.ericsoares.springmvc.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.ericsoares.springmvc.domain.Album;
+import br.com.ericsoares.springmvc.repositories.AlbumRepository;
 
 @Controller
 @RequestMapping("/albuns")
 public class AlbunsController {
 
+	@Autowired
+	private AlbumRepository repo;
+	
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(Model model) {
+		List<Album> albuns = repo.findAll();
+		model.addAttribute("albuns", albuns);
+		return "album.listar.tiles";
+ 	}
+	
 	@RequestMapping(value = "/adicionar", method = RequestMethod.GET)
 	public String adicionar(Model model) {
 		model.addAttribute("album", new Album());
@@ -26,7 +41,12 @@ public class AlbunsController {
 		if(result.hasErrors()) {
 			return "album.adicionar.tiles";
 		}
-		model.addAttribute("album", novoAlbum);
-		return "album.exibir.tiles";
+		repo.save(novoAlbum);	
+		return "redirect/albuns/listar";
+	}
+	
+	@RequestMapping(value = "/alterar/{id}", method = RequestMethod.POST)
+	public String alterar(@PathVariable("id") Long id, Model model) {
+		// TODO
 	}
 }
