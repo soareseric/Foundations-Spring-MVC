5d88dd71610387994fbea48376c8756edf001bc2
soareseric <contato.eric.sm@gmail.com>
2018-12-05 16:47:22 -0200

    Criando nosso repository

---
+++ src/br/com/ericsoares/springmvc/repositories/AlbumRepository.java
@@ -0,0 +1,10 @@
+package br.com.ericsoares.springmvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.ericsoares.springmvc.domain.Album;
+
+// A JPA IRÁ CRIAR UMA CLASSE EM TEMPO DE EXECUÇÃO PARA OS MÉTODOS CONTIDOS NO JPAREPOSITORY
+public interface AlbumRepository extends JpaRepository<Album, Long>{
+
+}
